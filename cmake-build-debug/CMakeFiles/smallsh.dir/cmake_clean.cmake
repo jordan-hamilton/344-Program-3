0a611db836415a2bf3619001abed1a35f24bf7c9
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-11-19 19:42:42 -0700

    Sync CLion settings

---
+++ cmake-build-debug/CMakeFiles/smallsh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smallsh.dir/smallsh.c.o"
+  "smallsh"
+  "smallsh.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/smallsh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
