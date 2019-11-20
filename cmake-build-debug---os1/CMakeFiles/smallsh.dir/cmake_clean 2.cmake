b9ce2d66cf824dff815d30fa0d7cff08d99a4027
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-11-19 20:38:08 -0700

    Start prompt

---
+++ cmake-build-debug---os1/CMakeFiles/smallsh.dir/cmake_clean 2.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/smallsh.dir/smallsh.c.o"
+  "smallsh.pdb"
+  "smallsh"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/smallsh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- smallsh.c
+++ smallsh.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main() {
-  printf("Hello, World!\n");
-  return 0;
+int main(int argc, char* argv[]) {
+  int shellPid = getpid();
+  int forkCount = 0, charsEntered = -5;
+  char* comment = "#";
+  char* exit = "exit";
+  char* command = NULL;
+  size_t commandSize = 0, commandMax = 2049;
+
+  command = malloc(commandMax * sizeof(char));
+  if (command == NULL)
+    perror("Allocating memory for a command failed");
+
+  do {
+    printf(":");
+    fflush(stdout);
+    charsEntered = getline(&command, &commandSize, stdin);
+    command[charsEntered - 1] = '\0';
+  } while (strcmp(command, exit) != 0);
+
+  return(0);
 }
